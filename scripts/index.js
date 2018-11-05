16d4bb82ca716f3fcf152730502ad0730c5e0b5e
superq <qq1143094348@gmail.com>
2018-11-05 23:21:53 +0800

    plugin commonjs

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,48 @@
 
 const rollup = require('rollup')
-const ts = require('rollup-plugin-typescript')
+const ts = require('rollup-plugin-typescript2')
 const resolve = require("rollup-plugin-node-resolve")
 const babel = require("rollup-plugin-babel")
+const convert2cjs = require('rollup-plugin-commonjs')
 
 const inputOptions = {
-  input: require.resolve("../src/index.tsx"),
+  input: require.resolve("../src/hello.jsx"),
   plugins: [
-    resolve(), 
+    convert2cjs({
+      exclude:['node_modules/**'],
+      namedExports: {
+        'node_modules/react/index': [
+          'React',
+          'cloneElement',
+          'createElement',
+          'PropTypes',
+          'Children',
+          'Component',
+       ],
+        'node_modules/react-dom/index.js': ['render']
+      }
+    }),
+    resolve({
+      //jsnext: true,
+      //main: true,
+      //browser: true
+    }),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     }),
-    ts(),
+    ts({
+      typescript: require('typescript'),
+    }),
   ]
 }
 const outputOptions = {
   name: "rc-lazyload-img",
-  file: "../lib/rc-lazyload-img.js",
+  file: "./lib/rc-lazyload-img.js",
   format: "cjs"
 }
 
 async function build() {
   const bundle = await rollup.rollup(inputOptions)
-  console.log(bundle)
   await bundle.write(outputOptions)
 }
 
