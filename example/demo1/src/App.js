760a1b57a4eeba4eed48481430e84a4a84d3c158
superq <qq1143094348@gmail.com>
2018-11-06 23:04:58 +0800

    modify demo1

--- example/demo1/src/App.js
+++ example/demo1/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as modules from 'rc-lazyload-img'
 
 class App extends Component {
   render() {
