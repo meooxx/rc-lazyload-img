3e4951d3771b3680c08ee475f2902b581faf7b68
superq <qq1143094348@gmail.com>
2018-11-20 00:06:05 +0800

    tag v1.0.0

--- example/demo1/src/App.js
+++ example/demo1/src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import jsLog from './js.jpeg'
+
 
 import './App.css';
 import  LoadImg  from 'rc-lazyload-img'
 
+import pic1 from './pic1.jpeg'
+import pic2 from './pic2.jpeg'
+import pic3 from './pic3.jpeg'
+import logo from './logo.svg';
+import jsLogo from './js.jpeg'
+
 class App extends Component {
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <div style={{background: "red", width:"100%", height: "900px"}}></div>
-          <LoadImg src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {
+            
+          }
+          
+        </ul>
 
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <div style={{background: "#456543", width:"100%", height: "900px"}}></div>
-
-          <LoadImg width="20px" height="20px" dataBGImg={jsLog} className="App-logo" alt="logo" />
-
-        </header>
       </div>
     );
   }
