82e18273879971c3272ca3cea9df9101012cfe64
superq <qq1143094348@gmail.com>
2018-11-20 23:55:21 +0800

    build demo1

--- example/demo1/src/App.js
+++ example/demo1/src/App.js
@@ -4,11 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import  LoadImg  from 'rc-lazyload-img'
 
-import pic1 from './pic1.jpeg'
-import pic2 from './pic2.jpeg'
-import pic3 from './pic3.jpeg'
-import logo from './logo.svg';
-import jsLogo from './js.jpeg'
+console.log(LoadImg)
 
 class App extends Component {
   render() {
