df4c22123fa1e869de3ba4ca36cb5cfa0610b1c8
superq <qq1143094348@gmail.com>
2018-11-21 23:54:27 +0800

    publishTo pages

--- scripts/release/pushToGhPage.js
+++ scripts/release/pushToGhPage.js
@@ -1,13 +1,71 @@
 
 
-// const cp = require('child_process')
+const cp = require('child_process')
 const fse = require('fs-extra')
-
+const {asyncRimraf} = require('../utils')
 
 const copyToPage = () => {
   fse.copySync("./example/demo1/build", './gh-pages')
 }
 
-module.exports = {
-  copyToPage
+
+const rootDir = process.cwd()
+const gitAddr = `git@github.com:meooxx/rc-lazyload-img.git`
+
+
+const pushToPage = async () => {
+  try {
+    await asyncRimraf(`${rootDir}/gh-pages`)
+  }catch(err) {
+    console.log(err + "\n")
+    console.log('gh-pages, 已经不存在')
+  }
+
+  await copyToPage()
+
+  const sub = cp.spawn(
+    'bash', 
+    {
+      cwd: `${rootDir}/gh-pages`
+
+    }
+  )
+  try {
+    await sub.stdin.write(`
+      pwd \n
+      git init \n
+      git remote add origin ${gitAddr} \n
+      git add . \n
+      git commit -m "gh-pages" \n
+      git status \n
+      git push origin HEAD:gh-pages \n
+    `)
+    await  sub.stdin.end()
+
+  }catch(err) {
+    console.log(err)
+  }
+  
+
+
+  sub.stdio.forEach(io=>io.setEncoding('utf-8'))
+  sub.stdout.on("data", (data) => {
+    console.log(data)
+  })
+
+  
+  sub.stderr.on('data', data=>{
+    console.log('stderr: ', data)
+    process.exit(1)
+
+  })
+
+   sub.on("exit", (...rest)=>console.log(rest))
+  
+
+
 }
+
+
+
+pushToPage()
